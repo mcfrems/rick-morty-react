e937ca719073279cc240e4879d22975401f5fc6f
mcfrems <matthew.fremstad@gmail.com>
2023-02-10 11:06:47 -0600

    css revisions

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -11,7 +11,7 @@ function About() {
   return (
     <div className={darkMode?'about-container about-dark' : 'about-container' }>
         <h1>About Us</h1>
-        <img src={banner} />
+        <img src={banner} className="about-banner"/>
         <p>
             Rick and Morty is an American adult animated science-fiction sitcom created by Justin Roiland and Dan Harmon for Cartoon Network's nighttime programming block Adult Swim. It is distributed internationally by Warner Bros. Domestic Television. The series follows the misadventures of cynical mad scientist Rick Sanchez and his good-hearted but fretful grandson Morty Smith, who split their time between domestic life and interdimensional adventures that take place across an infinite number of realities, often travelling to other planets and dimensions through portals and on Rick's flying saucer. The general concept of Rick and Morty relies on two conflicting scenarios: domestic family drama, and an alcoholic grandfather dragging his grandson into high jinks.
         </p>
